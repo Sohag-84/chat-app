eaca70ade8fab89a32c5fa41419eec3e6efa318e
Sohag <ihsohag-84@gmail.com>
2023-07-22 16:32:31 +0600

    show dots indicator in welcome page

--- lib/pages/welcome/controller.dart
+++ lib/pages/welcome/controller.dart
@@ -1,3 +1,11 @@
+import 'package:chat_application/pages/welcome/index.dart';
 import 'package:get/get.dart';
 
-class WelcomeController extends GetxController{}
+class WelcomeController extends GetxController{
+  final state = WelcomeSate();
+  WelcomeController();
+
+  changePage(int index)async{
+    state.index.value = index;
+  }
+}
--- lib/pages/welcome/view.dart
+++ lib/pages/welcome/view.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:chat_application/pages/welcome/controller.dart';
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
@@ -11,85 +12,105 @@ class WelcomePage extends GetView<WelcomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        height: 780.h,
-        width: 360.w,
-        child: Stack(
-          alignment: Alignment.bottomCenter,
-          children: [
-            PageView(
-              scrollDirection: Axis.horizontal,
-              reverse: false,
-              onPageChanged: (index) {},
-              controller: PageController(
-                initialPage: 0,
-                keepPage: false,
-                viewportFraction: 1,
-              ),
-              pageSnapping: true,
-              physics: ClampingScrollPhysics(),
-              children: [
-                Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: AssetImage("assets/images/banner1.png"),
-                    ),
-                  ),
+      body: Obx(() {
+        return SizedBox(
+          height: 780.h,
+          width: 360.w,
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              PageView(
+                scrollDirection: Axis.horizontal,
+                reverse: false,
+                onPageChanged: (index) {
+                  controller.changePage(index);
+                },
+                controller: PageController(
+                  initialPage: 0,
+                  keepPage: false,
+                  viewportFraction: 1,
                 ),
-                Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: AssetImage("assets/images/banner2.png"),
+                pageSnapping: true,
+                physics: ClampingScrollPhysics(),
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: double.infinity,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        fit: BoxFit.fill,
+                        image: AssetImage("assets/images/banner1.png"),
+                      ),
                     ),
                   ),
-                ),
-                Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: AssetImage("assets/images/banner3.png"),
+                  Container(
+                    width: double.infinity,
+                    height: double.infinity,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        fit: BoxFit.fill,
+                        image: AssetImage("assets/images/banner2.png"),
+                      ),
                     ),
                   ),
-                  child: Stack(
-                    alignment: Alignment.bottomCenter,
-                    children: [
-                      Positioned(
-                        bottom: 90.h,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.all(Colors.white),
-                            foregroundColor:
-                                MaterialStateProperty.all(Colors.black),
-                            shape: MaterialStateProperty.all(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.r),
+                  Container(
+                    width: double.infinity,
+                    height: double.infinity,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        fit: BoxFit.fill,
+                        image: AssetImage("assets/images/banner3.png"),
+                      ),
+                    ),
+                    child: Stack(
+                      alignment: Alignment.bottomCenter,
+                      children: [
+                        Positioned(
+                          bottom: 90.h,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(Colors.white),
+                              foregroundColor:
+                                  MaterialStateProperty.all(Colors.black),
+                              shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.r),
+                                ),
+                              ),
+                              side: MaterialStateProperty.all(
+                                BorderSide(color: Colors.white),
                               ),
                             ),
-                            side: MaterialStateProperty.all(
-                              BorderSide(color: Colors.white),
-                            ),
+                            child: Text("Login"),
                           ),
-                          child: Text("Login"),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Positioned(
+                bottom: 70.h,
+                child: DotsIndicator(
+                  position: controller.state.index.value,
+                  dotsCount: 3,
+                  reversed: false,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  decorator: DotsDecorator(
+                    size: Size.square(9),
+                    activeSize: Size(18, 9),
+                    activeShape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.r),
+                    ),
                   ),
                 ),
-              ],
-            ),
-          ],
-        ),
-      ),
+              ),
+            ],
+          ),
+        );
+      }),
     );
   }
 }
